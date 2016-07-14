acfeb48866747c7ff6f16abe3a69bfc455ea7ac9
Hana Worku <haworku@gmail.com>
2016-07-14 22:49:22 +0300

    Map out listeners for swapping between mini and full skin. Also, comments with future todos.

--- js/events.js
+++ js/events.js
@@ -17,5 +17,26 @@ APP.attachListeners = function(){
     APP.view.loop(!APP.state.loop);
     APP.state.loop = !APP.state.loop;
   });
+
+  APP.view.mini.addEventListener('click', function(e){
+    APP.view.swapSkin('mini');
+  });
+
+  APP.view.full.addEventListener('click', function(e){
+   APP.view.swapSkin('full');
+  });
+
+// TO DO 
+// volume drag and tracking drag- player.volume or player.currentTime should update
+// next and previous - state.currentTrack, state.completeQue, and state.nextQue should update
+// any click on trackList-  state.currentTrack, state.completedQue, state.nextQue should update
+
+
+// ALSO, BACKGROUND LISTENERS
+// player/web audio listener for song complete - state.currentTrack, state.completedQue, state.nextQue should update
+// state listener for any changes on state.completedQue - renderTrackList() based on changes, IF length == 0 and state.loop == true, move completeQue to nextQue and render trackList
+// state listener for any change on state.currentTrack - adjust player.track, then player.play(state.playing) and view.renderTrack()
+};
+
 // APP.attachListeners(); 
 // 
