f683f5a515a64fcf9b2c22263cc47668831e8ae0
Hana Worku <haworku@gmail.com>
2016-07-14 22:53:16 +0300

    Establish CSS selector naming following hios pattern as well as matching property in View object

--- js/view.js
+++ js/view.js
@@ -6,23 +6,61 @@ View.prototype.buildHTML = function () {
   document.querySelector('body').appendChild('<div id="hios-main-container"></div>');
   document.querySelector('hios-app-container').innerHTML = this.HTML;
 };
+
+View.protoype.addSelectors = function(){
+  this.mini = document.querySelector('#hios-mini');
+  this.full = document.querySelector('#hios-full');
+
+  this. controls = {
+    playPause:  document.querySelector('.hios-play-pause'),
+    next: document.querySelector('.hios-next'),
+    previous: document.querySelector('.hios-previous'),
+    shuffle: document.querySelector('.hios-shuffle'),
+    loop: document.querySelector('.hios-loop'),
+    volumeSlider: document.querySelector('.hios-volume'),
+    trackingSlider: document.querySelector('.hios-tracking'),
+    mini: document.querySelector('#hios-minify'),
+    full: document.querySelector('#hios-fullify'),
+    trackList: document.querySelectorAll('.track')
+  };
+
+  this.trackInfo = {
+    title: document.querySelector('.hios-song-title'),
+    artist: document.querySelector('.hios-song-artist'),
+    image: document.querySelector('.hios-thumbnail')
+  };
 };
 
-Viewz.prototype.display = function(state) {
-  Viewz.state = state;
-
-  switch (state) {
-    case 'MINI':
-          this.container.querySelector('.mini');
-    break;
-
-    case 'FULL':
-          this.container.querySelector('.full');
-    break;
-
-    default:
-      console.warn('not a valid state');
-    break;
-  }
+View.prototype.play = function (bool, options){
+  // document.querySelector('.play-pause') swap between play and pause based on bool
+  // if options {repeat: true} restart currentTrack
 };
 
+View.prototype.shuffle = function (bool){
+  // toggle higlight class on shuffle button
+};
+
+View.prototype.loop = function (bool){
+  // toggle higlight class on loopbutton
+};
+
+
+View.prototype.swapSkin = function(cssSelector) {
+  // fade out .active skin, add .active class to document.querySelector(cssSelector)
+};
+
+View.prototype.adjustVolumeSlider = function(number) {
+  // move volume to number location
+};
+
+View.prototype.adjustTrackingSlider = function(number) {
+  // move tracking to number location
+};
+
+View.prototype.renderTrackList= function(currentTrack) {
+  // rerender currentTrack title, image, artice
+};
+
+View.prototype.renderCurrentTrack = function(trackList) {
+  // rerender tracks in trackList order
+};
