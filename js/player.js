4cb0edaac2a8f19310e196241f599fd571991008
Hana Worku <haworku@gmail.com>
2016-07-14 22:52:01 +0300

    Refactor audio player next/previous/play/pause/shuffle functions - all will simply use .play(bool, options) function.

--- js/player.js
+++ js/player.js
@@ -3,40 +3,17 @@ var AudioPlayer = function(currentTrack) {
   this.track = currentTrack;
 };
 
-AudioPlayer.prototype.navigate = function(direction) {
-  var currentTrackIndex;
-  switch (direction) {
-    case 'RANDOM':
-      currentTrackIndex =  Math.floor(Math.random() * (currentAlbum.length));
-    break;
 
-    case 'NEXT':
-      if (currentTrackIndex + 1 >= currentAlbum.length) {
-        currentTrackIndex = 0;
-      } else {
-        currentTrackIndex++;
-      }
-    break;
-
-    case 'PREV':
-      if (currentTrackIndex === 0) {
-        currentTrackIndex = currentAlbum.length - 1;
-      } else {
-        currentTrackIndex--;
-      }
-    break;
-
-    default:
-      console.warn('me not know');
-    break;
-  }
+AudioPlayer.prototype.play = function(bool, options) {
+// if true play if false pause
+// options = {fromBeginning: true} used to start over track on first previous click
 };
 
-AudioPlayer.prototype.play = function() {
+AudioPlayer.prototype.adjustVolume = function(newVolume){
 
 };
 
-AudioPlayer.prototype.pause = function() {
+AudioPlayer.prototype.scrubTo = function(newTime) {
 
 };
- 
+
