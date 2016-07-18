fe548b65451057005ee076b679dd05af5947f4ae
Hana Worku <haworku@gmail.com>
2016-07-18 18:03:38 +0300

    Working on the event handling
    - audio player events (play, pause, ended, timeupdate, volumechange)
    -emitting and handling events from main app

--- js/events.js
+++ js/events.js
@@ -57,7 +57,27 @@ APP.attachListeners = function(){
 // volume drag and tracking drag- player.volume or player.currentTime should update
 // any click on trackList-  state.currentTrack, state.completedQue, state.nextQue should update
 
+// LISTENING FOR: AUDIO PLAYER EVENTS
+ APP.player.audio.addEventListener('play', (e) =>  {
+    console.log(e);
+  });
 
+  APP.player.audio.addEventListener('pause', (e) =>  {
+    console.log(e);
+  });
+
+  APP.player.audio.addEventListener('ended', (e) => {
+    console.log(e);
+  });
+
+  APP.player.audio.addEventListener('timeupdate', (e) => {
+    console.log(e);
+    console.log(APP.player.audio.currentTime);
+  });
+
+  APP.player.audio.addEventListener('volumechange', (e) => {
+    console.log(e);
+  });
 // ALSO, BACKGROUND LISTENERS
 // player/web audio listener for song complete - state.currentTrack, state.completedQue, state.nextQue should update
 // state listener for any changes on state.completedQue - renderTrackList() based on changes, IF length == 0 and state.loop == true, move completeQue to nextQue and render trackList
@@ -65,4 +85,24 @@ APP.attachListeners = function(){
 };
 
 APP.attachListeners(); 
+
+APP.handleEvent =function (event) {
+  switch event{
+    case 'play':
+
+      break;
+    case 'pause':
+      break;
+    case 'ended':
+      break;
+    case 'timeupdate':
+      break;
+    case 'volumechange':
+      break;
+
+    default: 
+      console.log('default')
+      break;
+  }
+}
 // 
