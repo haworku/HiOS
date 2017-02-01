aadcbef21a4a1642ca5e93ec5f10965295f95d90
haworku <haworku@gmail.com>
2017-02-01 15:52:50 -0800

    hiosEvents cleanup

--- js/redux-events.js
+++ js/redux-events.js
@@ -17,49 +17,22 @@ hiosEvents = () => {
 		      break;
 
 		    case 'next':
-		      // if triggered by click on track list, jump to that track
-		      // if (target && target.getAttribute('class') == 'hios-track') {
-		      //   var index = APP.view.getTrackIndex(target);
-		      //   var oldIndex = APP.state.currentTrack.id - 1;
-		      //   APP.state.completeQue.unshift(APP.state.currentTrack);
-		      //   APP.state.currentTrack =  APP.state.music[index];
-		      //   APP.state.nextQue = APP.state.nextQue.splice(oldIndex + (index - oldIndex), APP.state.nextQue.length -1);
-		      //   APP.player.update({time: 0, source: APP.state.currentTrack.source});
-		      //   APP.view.updateTrackList({track: APP.state.currentTrack});
-		      // } else if (APP.state.nextQue.length > 0) { 
-		      // // if more songs left
-		      //   APP.state.completeQue.unshift(APP.state.currentTrack);
-		      //   APP.state.currentTrack =  APP.state.nextQue.shift();
-		      //   APP.player.update({time: 0, source: APP.state.currentTrack.source});
-		      //   APP.view.updateTrackList({track: APP.state.currentTrack});
-		      // } else if (APP.state.loopAll) {
-		      //  //if no more songs left but looping playlist
-		      //   APP.reset();
-		      //   APP.view.updateTrackList({music: APP.state.nextQue, track: APP.state.currentTrack});
-		      //   APP.player.update({source:APP.state.currentTrack.source, volume: .5, currentTime: 0});
-		      // } else {
-		      //     console.log('no more songs');
-		      // }
-
+		      store.dispatch({type:'NEXT', track:})
+		       store.dispatch({type: 'UPDATE_AUDIO', tracking: 0 }) 
 		      break;
 
 		    case 'previous':
-		      // if (APP.state.completeQue.length === 0 || APP.player.justStarted() === false ){
-		      //   APP.player.update(APP.state.playing, {time: 0}); // replay current song
-		      // } else {
-		      //   APP.state.nextQue.unshift(APP.state.currentTrack);   // go back one song
-		      //   APP.state.currentTrack =  APP.state.completeQue.shift();
-		      //   APP.view.updateTrackList({track: APP.state.currentTrack});
-		      //   APP.player.update({time: 0, source: APP.state.currentTrack.source});
-		      // }
+		      store.dispatch({type: 'PREVIOUS', tracking: 0 }) 
+		    	store.dispatch({type: 'UPDATE_AUDIO', tracking: 0 }) 
+		      break;
 
+		    case 'jump': 
+		        store.dispatch({type:'JUMP_TO', track:})
+		        store.dispatch({type: 'UPDATE_AUDIO', tracking: 0 }) 
 		      break;
 
 		    case 'shuffle':
 		      store.dispatch({type:'TOGGLE_SHUFFLE'})
-		      if (store.getState().shuffle) {
-		       // store.dispatch shuffle store.music 
-		      }
 		      break;
 
 		    case 'loop':
@@ -67,22 +40,22 @@ hiosEvents = () => {
 		      break;
 
 		    case 'tracking':  
-		     // audio player listeners also at work here
-		    	// get property directly from target ??? not need for reference to HTML
+		    	// audio player listeners also at work here
+		    	// get new tracking value directly from target 
 		      store.dispatch({type: 'UPDATE_AUDIO', tracking: 0 }) 
 		      break;
 
 		    case 'volume':
-		    // get property directly from target ??? no need for reference to HTML
+		   	 // get new volume value directly from target
 		      store.dispatch({type: 'UPDATE_AUDIO', volume: 0}) 
 		      store.dispatch({type: 'VOLUME', volume: 0 }) 
 		      break;
 
 		    case 'swap':
 		      if (target && target.getAttribute('id') == 'hios-minify'){
-		      	// dispatch event
+		      	store.dispatch({type: 'TOGGLE_PLAYER_TYPE'})
 		      }else{
-		      	// dispatch event
+		      	store.dispatch({type: 'TOGGLE_PLAYER_TYPE'})
 		      }
 		      break;
 
