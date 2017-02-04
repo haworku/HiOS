7429ca2a98eb3b5653f5e238bd684faa6b638c55
haworku <haworku@gmail.com>
2017-02-03 16:53:15 -0800

    playerReducer NEXT, TOGGLE_SHUFFLE, JUMP_TO logic -NEXT interacts with looping when nextQue runs out else continues to nextQue[0] -TOGGLE_SHUFFLE changes nextQue in advance of progressing to next song -JUMP_TO when track is clicked -need for utility methods to generate nextQue based on wether or not there is shuffle

--- js/redux-player.js
+++ js/redux-player.js
@@ -21,18 +21,41 @@ const playerReducer = (state = {}, action) => {
       return Object.assign( {}, state, {
         playing: !state.playing,
         });
+
     case 'NEXT':
+      if (state.nextQue.length > 0){ // go to next song
+        return Object.assign( {}, state, {
+          completeQue: state.completeQue.concat(state.currentTrack),
+          nextQue: action.nextQue.slice(1),
+          currentTrack:action.nextQue[0],
+        });
+      } else if (state.loopAll == true) { // set player to continue looping
+        return Object.assign( {}, state, {
+          completeQue: state.completeQue.concat(state.currentTrack),
+          nextQue: resetNextQue(state.shuffle, state.music),
+          currentTrack: state.completeQue[0], 
+        });
+      } else { //  reset player to start over NEED TO ALSO REFRESH DOM NO SONGS HIGHLIGHTED
+        return Object.assign( {}, state, {
+          completeQue: [],
+          nextQue: action.music.slice(1),
+          currentTrack: action.music[0],
+          volume: .5,
+          playing: false,
+          shuffle: false,
+          loopCurrent: false,
+          loopAll: false,
+        });
+
+      }   
+
+    case 'JUMP_TO':
       return Object.assign( {}, state, {
         completeQue: state.completeQue.concat(state.currentTrack),
-        nextQue: state.nextQue.slice(1),
-        currentTrack: state.nextQue[0]
+        nextQue: resetNextQue(state.shuffle, state.nexQue.slice(1)),
+        currentTrack: state.music[action.jumpToIndex],
       });
-      // if more songs left go to next song
-      // no more songs left but looping playlist start over
-      // if no more songs left and not looping stop playing
-      return state;
-    case 'JUMP_TO':
-      return state;
+
     case 'PREVIOUS':
       if (state.audioObject.currentTime < 5){
         return Object.assign( {}, state.audioObject, { currentTime: 0});
@@ -43,7 +66,8 @@ const playerReducer = (state = {}, action) => {
           currentTrack: state.completeQue[-1]
         });
       }
-      return state;
+
+
     case 'UPDATE_AUDIO':
       return Object.assign( {}, state, {
         audioObject: Object.assign( {}, state.audioObject, {
@@ -57,11 +81,12 @@ const playerReducer = (state = {}, action) => {
       return Object.assign( {}, state, {
         volume: action.volume
         }); 
-      return state
+
 
     case 'TOGGLE_SHUFFLE':
       return Object.assign( {}, state, {
-        shuffle: !state.shuffle
+        shuffle: !state.shuffle, 
+        nextQue: shuffleNextQue(state.shuffle, state.music),
       });
 
     case 'TOGGLE_LOOP':
@@ -74,7 +99,7 @@ const playerReducer = (state = {}, action) => {
         fullPlayer: !state.fullPlayer
       });
 
-      return state;
+   
     default:
       return state;
   }
