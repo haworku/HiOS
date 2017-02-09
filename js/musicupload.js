6eab30ac8e357b20df227460359ec066881260ec
haworku <haworku@gmail.com>
2017-02-08 17:46:53 -0800

    Music id3 upload progress -seems to launch player properly (music has tracks populated with info from id3 tags) -need to fix console.log trickle down error to be fully functional

--- js/musicupload.js
+++ js/musicupload.js
@@ -7,39 +7,48 @@
   let loadMusic = function(e){
     let defaults = {title: 'Untitled', artist: 'Unknown Artist', album: 'Unknown Album', image:'/static/images/album_cover_Andrew_Fortnum.png' };
     let files = this.files;
-      
-    let promise = new Promise(function(resolve, reject){
-    var music = ['fix me'];
+    let tracks = [];
 
-      Object.keys(files).forEach( (key) => {
-        id3( files[key], function(err, id3Tags) {
-          let tags =  {
-            title: id3Tags.title || defaults.title, 
-            artist: id3Tags.artist || defaults.artist,
-            image: '' || defaults.image,
-            album: id3Tags.album || defaults.album
-          }
-          music.push(
-            { 'id': Number(key) + 1,
+    let launchPromise = new Promise(function(resolve, reject){ 
+      console.log('ran launch', tracks)
+      (tracks.length > 0) ? resolve() : reject();
+    });
+
+
+    let fn = function asyncTrackGenerator(fileKey){ // sample async action
+      let tags = {};
+
+      id3( files[fileKey], function(err, id3Tags) {
+        tags =  {
+          title: id3Tags.title || defaults.title, 
+          artist: id3Tags.artist || defaults.artist,
+          image: '' || defaults.image,
+          album: id3Tags.album || defaults.album
+        }
+      }) // ./ id3  
+
+        return new Promise(resolve => setTimeout(() => {
+          tracks.push(
+            { 'id': Number(fileKey) + 1,
               'title': tags.title,
               'artist': tags.artist,
               'album': tags.album,
-              'source': URL.createObjectURL(files[key]),
+              'source': URL.createObjectURL(files[fileKey]),
               'image': tags.image
             }
-          );
-        }) // ./ id3   
-      }) // ./ forEach
-      console.log('fix me', music, music.length)
-    
-      (music.length > 0) ? resolve(music) : reject();
+          );  
 
-    }); // ./ promise declaration
+          resolve(tracks)
+         }, 100)); // ./ track promise resolve
+    };
 
-      promise
-      .then(function(music) { 
-        APP.launch(music) 
-      })
+    let fileActions = Object.keys(files).map(fn); // returns all track promises
+
+    let generatorComplete = Promise.all(fileActions); // wait for all promises to resolve
+
+    generatorComplete
+      .then(launchPromise)
+      .then(APP.launch(tracks))
       .catch(function(error) {
         console.log(Error('Something went wrong cannot launch player'))  
       });
