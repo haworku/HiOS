162bf624576d6cb2278afc19fa41adc2fafe0623
haworku <haworku@gmail.com>
2017-02-08 16:51:02 -0800

    id3 tags almost working -need to fix closures/async issue with passing music APP.launch()

--- js/musicupload.js
+++ js/musicupload.js
@@ -1,30 +1,50 @@
 (function loadLocalAudio() {
   'use strict';
 
-  var URL = window.URL || window.webkitURL;
+  let URL = window.URL || window.webkitURL;
+  let reader = new FileReader();
 
-  var loadMusic = function (e){
-    let music = [];
+  let loadMusic = function(e){
+    let defaults = {title: 'Untitled', artist: 'Unknown Artist', album: 'Unknown Album', image:'/static/images/album_cover_Andrew_Fortnum.png' };
+    let files = this.files;
+      
+    let promise = new Promise(function(resolve, reject){
+    var music = ['fix me'];
 
-    var files = this.files;
+      Object.keys(files).forEach( (key) => {
+        id3( files[key], function(err, id3Tags) {
+          let tags =  {
+            title: id3Tags.title || defaults.title, 
+            artist: id3Tags.artist || defaults.artist,
+            image: '' || defaults.image,
+            album: id3Tags.album || defaults.album
+          }
+          music.push(
+            { 'id': Number(key) + 1,
+              'title': tags.title,
+              'artist': tags.artist,
+              'album': tags.album,
+              'source': URL.createObjectURL(files[key]),
+              'image': tags.image
+            }
+          );
+        }) // ./ id3   
+      }) // ./ forEach
+      console.log('fix me', music, music.length)
+    
+      (music.length > 0) ? resolve(music) : reject();
 
-    Object.keys(files).forEach( function (key){
-      music.push(
-        { 'id': Number(key) + 1,
-          'title': files[key].name.substr(0, files[key].name.lastIndexOf('.')) || files[key].name,
-          'artist': 'Beyonce',
-          'source': URL.createObjectURL(files[key]),
-          'image': '/static/images/album_cover_Andrew_Fortnum.png'
-        }
-      );
-    });
+    }); // ./ promise declaration
 
-    if (music.length == 0){  // if no valid audio files use static
-      console.log('upload failed')
-    }
-    APP.launch(music)
-  };
+      promise
+      .then(function(music) { 
+        APP.launch(music) 
+      })
+      .catch(function(error) {
+        console.log(Error('Something went wrong cannot launch player'))  
+      });
+  }
 
-  var inputNode = document.querySelector('input')
+  let inputNode = document.querySelector('input')
   inputNode.addEventListener('change', loadMusic, false)
 })()
