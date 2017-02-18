f73e9b2bad9ae5fe9beba26b19293a15ceab4e34
haworku <haworku@gmail.com>
2017-02-18 09:00:35 -0800

    Add back old js and dist to git

---
+++ js/old_js/events.js
@@ -0,0 +1,180 @@
+console.log('loading events');
+APP.attachListeners = function(e){
+  'use strict';
+  APP.mousedown = false;
+  // USER MANIPULATION
+  var container = APP.view.getContainer();
+  var mini = APP.view.getMini();
+
+  document.addEventListener('click', function (e){
+    if( !e.target.getAttribute('data-state') ){
+      APP.handleEvent('swap');
+    }else{
+      APP.handleEvent(e);
+    }
+  }, false);
+
+  container.addEventListener('mousedown', function (e){
+    console.log('mousedown');
+    APP.mousedown = true;
+  }, false);
+
+  container.addEventListener('mouseup', function (e){
+    APP.mousedown = false;
+  }, false);
+
+  mini.addEventListener('dragstart', function(e){
+    APP.view.dragging(e, 'start');
+  }, false);
+
+  container.addEventListener('dragover', function(e){
+    e.preventDefault();
+    if (e.stopPropagation) { e.stopPropagation(); }
+    e.dataTransfer.dropEffect = 'move';
+  }, false);
+
+  container.addEventListener('drop', function(e){
+    e.preventDefault();
+    if (e.stopPropagation) { e.stopPropagation(); }
+    APP.view.dragging(e, 'drop');
+  }, false);
+
+
+
+
+  // AUDIO PLAYER
+  APP.player.audio().addEventListener('loadedmetadata', function(e){
+    // wait to set tracking until duration data loads
+    APP.view.resetTracking(parseInt(APP.player.audio().duration,10));
+  });
+
+  APP.player.audio().addEventListener('ended', function(e) {
+    APP.handleEvent('next');
+  });
+
+  APP.player.audio().addEventListener('timeupdate', function(e){
+    // dynamically update tracking as song plays
+    if (APP.mousedown == false)
+      APP.view.tracking(
+        parseInt(APP.player.audio().currentTime, 10), parseInt(APP.player.audio().duration, 10)
+      );
+  });
+
+};
+
+APP.handleEvent = function (e) {
+  console.log('music', APP.state.music);
+  if (e.preventDefault) e.preventDefault();
+
+  // event element  - will be undefined if parameter is not event object
+  var target = e.target || e.srcElement;
+
+  // if event, get data-state, else directly assign parameter to string variable
+  var string = target ? target.getAttribute('data-state') : e;
+
+  switch (string){
+    case 'playpause':
+      if (APP.state.playing) {
+        APP.view.play(false);
+      } else {
+        APP.view.play(true);
+      }
+      APP.state.playing = !APP.state.playing;
+
+      break;
+
+    case 'next':
+      // if triggered by click on track list, jump to that track
+      if (target && target.getAttribute('class') == 'hios-track') {
+        var index = APP.view.getTrackIndex(target);
+        var oldIndex = APP.state.currentTrack.id - 1;
+        APP.state.completeQue.unshift(APP.state.currentTrack);
+        APP.state.currentTrack =  APP.state.music[index];
+        APP.state.nextQue = APP.state.nextQue.splice(oldIndex + (index - oldIndex), APP.state.nextQue.length -1);
+        APP.player.update({time: 0, source: APP.state.currentTrack.source});
+        APP.view.updateTrackList({track: APP.state.currentTrack});
+      } else if (APP.state.nextQue.length > 0) { // if more songs left
+        APP.state.completeQue.unshift(APP.state.currentTrack);
+        APP.state.currentTrack =  APP.state.nextQue.shift();
+        APP.player.update({time: 0, source: APP.state.currentTrack.source});
+        APP.view.updateTrackList({track: APP.state.currentTrack});
+      } else if (APP.state.loopAll) { //if no more songs left but looping playlist
+        APP.reset();
+        APP.view.updateTrackList({music: APP.state.nextQue, track: APP.state.currentTrack});
+        APP.player.update({source:APP.state.currentTrack.source, volume: .5, currentTime: 0});
+      } else {
+          console.log('no more songs');
+      }
+
+      break;
+
+    case 'previous':
+      if (APP.state.completeQue.length === 0 || APP.player.justStarted() === false ){
+        APP.player.update(APP.state.playing, {time: 0}); // replay current song
+      } else {
+        APP.state.nextQue.unshift(APP.state.currentTrack);   // go back one song
+        APP.state.currentTrack =  APP.state.completeQue.shift();
+        APP.view.updateTrackList({track: APP.state.currentTrack});
+        APP.player.update({time: 0, source: APP.state.currentTrack.source});
+      }
+
+      break;
+
+    case 'shuffle':
+      APP.state.shuffle = !APP.state.shuffle;
+      APP.view.shuffle(APP.state.shuffle);
+      if (APP.state.shuffle) {
+        APP.reset();
+        APP.state.nextQue = APP.state.nextQue.hiosShuffle();
+      } else {
+        APP.view.updateTrackList({track: APP.state.currentTrack, music: APP.state.music});
+      }
+      break;
+
+    case 'loop':
+      if (!APP.state.loopCurrent && !APP.state.loopAll){
+        // first click loops current track
+        APP.state.loopCurrent = true;
+        APP.player.audio().loop = APP.state.loopCurrent;
+        APP.view.loop('current');
+      } else if (!APP.state.loopAll) {
+      // second click loops entire playlist
+        APP.state.loopCurrent = false;
+        APP.state.loopAll = true;
+        APP.view.loop('all');
+        // APP.view.loopAll(APP.state.loopAll);
+        console.log(APP.state.loopAll);
+      }else{
+        //third click turns off all looping
+        APP.state.loopCurrent = false;
+        APP.state.loopAll = false;
+        APP.view.loop(null);
+      }
+
+      break;
+
+    case 'tracking':  // audio player listneners also at work here
+        APP.player.audio().currentTime = APP.view.getSelectorProperty('tracking', 'value');
+
+      break;
+
+    case 'volume':
+      APP.player.audio().volume = APP.view.getSelectorProperty('volume', 'value');
+      APP.state.volume = APP.player.audio().volume;
+
+      break;
+    case 'swap':
+      if (target && target.getAttribute('id') == 'hios-minify'){
+        APP.view.swapSkin('mini');
+      }else{
+        APP.view.swapSkin('full');
+      }
+
+      break;
+
+    default:
+      console.log('default');
+      break;
+  }
+  APP.player.play(APP.state.playing); //player remains played or paused
+};
---
+++ js/old_js/view.js
@@ -0,0 +1,287 @@
+'use strict';
+var hiosView = function () {
+  var selectors, appHTML, trackHTML, getTime, populateCurrentTrack;
+
+  selectors = {};
+
+  appHTML =
+    `
+      <div id="hios-mini" draggable="true" class="hios-active animated slideInUp">
+        <div class="hios-wrap mini">
+          <img class="hios-thumbnail" src="/static/images/lemonade.jpg">
+          <div class="hios-song-title-box">
+             <div class="hios-song-title"></div>
+          </div>
+          <div class="hios-audio mini">
+              <i class="hios-play-pause icon-play" data-state="playpause" ></i>
+              <i class="hios-next icon-skip-forward" data-state="next"></i>
+          </div>
+        </div>
+      </div>
+
+      <div id="hios-full" class="hios-inactive">
+        <i id="hios-minify" class="icon-shrink" data-state="swap"></i>
+        <div class="hios-artwork-container">
+          <img class="hios-artwork" src="/static/images/lemonade.jpg">
+        </div>
+        <div class="hios-controls full">
+          <div class ="hios-progress">
+            <input
+              id= "hios-progress-tracking"
+              class="hios-tracking"
+              type="range"
+              data-state="tracking"
+              value= "0" min="0" max="100"
+            >
+            <div id="tracking-output">
+              <output
+                for="tracking"
+                id="hios-progress-completed">
+                1.00
+              </output>
+              <output
+                for="tracking"
+                id="hios-progress-duration">
+                10.00
+              </output>
+            </div>
+          </div>
+
+          <div class ="hios-info full">
+            <div class="hios-song-title"></div>
+            <div class="hios-song-artist"></div>
+          </div>
+
+          <div class="hios-audio full">
+            <i class="hios-control hios-previous icon-skip-back" data-state="previous" ></i>
+            <i class="hios-control hios-play-pause icon-play" data-state="playpause" ></i>
+            <i class="hios-control hios-next icon-skip-forward" data-state="next"></i>
+          </div>
+
+          <div class="hios-volume" full>
+            <input
+                id="hios-volume-tracking"
+                class="hios-tracking"
+                type="range"
+                data-state="volume"
+                value=".5" min="0" max="1" step= ".01"
+              >
+          </div>
+
+          <div class="hios-playback full">
+            <i class="hios-control hios-shuffle icon-shuffle" data-state="shuffle"></i>
+            <i class="hios-control hios-loop icon-repeat" data-state="repeat"></i>
+          </div>
+
+        </div>
+        <div id="hios-track-list">
+        </div>
+      </div>
+    `;
+
+  trackHTML =
+    `
+      <img class="hios-thumbnail" src="/static/images/lemonade.jpg">
+      <div class="hios-info">
+        <div class="hios-song-title slideInRight">adfd</div>
+        <div class="hios-song-artist slideInLeft">dasfasdf</div>
+      </div>
+    `;
+
+
+  getTime = function (t) {
+    var m=~~(t/60), s=~~(t % 60);
+    return (m<10?"0"+m:m)+':'+(s<10?"0"+s:s);
+  };
+
+  populateCurrentTrack = function (currentTrack) {
+    selectors.title[0].innerHTML = currentTrack.title;
+    selectors.title[1].innerHTML = currentTrack.title;
+    selectors.artist.innerHTML = currentTrack.artist;
+
+    selectors.thumbnail.forEach (function (img){
+      img.setAttribute('src', currentTrack.image);
+    });
+  };
+
+  return {
+    defineSelectors: function(){
+      var container = document.querySelector('#hios-app')
+
+      selectors = {
+        appContainer: container,
+        miniContainer: container.querySelector('#hios-mini'),
+        fullContainer: container.querySelector('#hios-full'),
+        playPause: [container.querySelector('.mini > .hios-play-pause'), container.querySelector('.full > .hios-play-pause')],
+        next: [container.querySelector('.mini > .hios-next'), container.querySelector('.full > .hios-next')],
+        previous: container.querySelector('.full > .hios-previous'),
+        shuffle: container.querySelector('.hios-shuffle'),
+        loop: container.querySelector('.hios-loop'),
+        volume: container.querySelector('#hios-volume-tracking'),
+        tracking: container.querySelector('#hios-progress-tracking'),
+        trackingTimeProgress: container.querySelector('#hios-progress-completed'),
+        trackingTimeDuration: container.querySelector('#hios-progress-duration'),
+        minify: container.querySelector('#hios-minify'),
+        fullify: container.querySelector('#hios-fullify'),
+        trackList: container.querySelector('#hios-track-list'),
+        title: [container.querySelector('.mini > .hios-song-title-box > .hios-song-title'), container.querySelector('.full > .hios-song-title')],
+        artist: container.querySelector('.full > .hios-song-artist'),
+        thumbnail: [container.querySelector('.mini > .hios-thumbnail'), container.querySelector('#hios-full > .hios-artwork-container > .hios-artwork')],
+        trackList: container.querySelector('#hios-track-list'),
+      };
+    },
+
+    getContainer: function (){
+      return selectors.appContainer;
+    },
+    getMini: function (){
+      return selectors.miniContainer;
+    },
+    getSelectorProperty: function (selector, property){
+      return  selectors[selector][property];
+    },
+    getTrackIndex : function (element){
+      return Array.prototype.indexOf.call(selectors.trackList.children, element);
+    },
+    buildHTML: function () {
+      document.querySelector('#hios-app').innerHTML = appHTML;
+    },
+    dragging : function (event, action){
+      console.log('in this')
+      var element = event.target;
+      switch (action){
+        case 'start':
+          console.log('start')
+          element.style.opacity ='0.4'
+          event.dataTransfer.effectAllowed = 'move';
+          event.dataTransfer.setData('text/html', element.innerHTML);
+        break;
+        case 'over':
+        console.log('over')
+          event.preventDefault();
+          event.dataTransfer.dropEffect = 'move';
+        break;
+
+        case 'drop':
+
+          console.log('drop')
+          event.stopPropagation();
+          // if element being dropped into is not is not hios-full adjust hios-mini css
+          var offset = event.dataTransfer.getData('text/plain').split(',');
+          var mini =  selectors.miniContainer;
+          mini.style.opacity ='1';
+          mini.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
+          mini.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
+        break;
+
+      }
+
+    },
+    /**
+     * resetTracking
+     * adjust tracking slider to beginning position and tracking duration time display
+     * called only once metadata has loaded
+     * @param  {Number} duration
+     */
+    resetTracking: function (duration) {
+      selectors.tracking.setAttribute('max', duration);
+      selectors.trackingTimeDuration.innerHTML = getTime(duration);
+      selectors.tracking.setAttribute('value', 0);
+    },
+    /**
+     * play
+     * switch play/pause button display
+     * @param  {Boolean} playing
+     */
+    play: function (playing){
+      if (playing === true) {
+        selectors.playPause[0].className = 'hios-play-pause icon-play';
+        selectors.playPause[1].className = 'hios-play-pause icon-play';
+      } else {
+        selectors.playPause[0].className = 'hios-play-pause icon-pause';
+        selectors.playPause[1].className = 'hios-play-pause icon-pause';
+      }
+    },
+    /**
+     * shuffle
+     * toggle shuffle button on and off with timeout
+     * @param  {Boolean} shuffling
+     */
+    shuffle: function (shuffling) {
+      selectors.shuffle.className = shuffling === true ? 'hios-shuffle hios-control icon-shuffle hios-activated' : 'hios-shuffle hios-control icon-shuffle';
+    },
+     /**
+     * loop
+     * toggle loop button depending on loop type, default: unactive
+     * @param  {String} loopType
+     */
+    loop: function (loopType){
+      switch (loopType){
+        case 'current':
+          console.log('loop current')
+          selectors.loop.className = 'hios-loop hios-activated icon-repeat'
+          break;
+        case 'all':
+          console.log('loop all')
+          selectors.loop.className = 'hios-loop hios-activated icon-repeat'
+          break;
+        default:
+          console.log('loop nothing')
+          selectors.loop.className = 'hios-loop icon-reeat'
+          break;
+      }
+    },
+    /**
+     * tracking
+     * adjusts tracking based on user manipulation OR timechange
+     * @param  {Number} currentTime, {Number} duration
+     */
+    tracking: function(currentTime, duration) {
+      selectors.tracking.value = currentTime;
+      selectors.tracking.setAttribute('value', currentTime);
+      selectors.trackingTimeProgress.innerHTML = getTime(currentTime);
+      selectors.trackingTimeDuration.innerHTML= '-' + getTime(duration-currentTime);
+    },
+    /**
+     * swapSkin
+     * changes display visibility of mini and full player
+     * @param  {String} skin
+     */
+    swapSkin: function(skin) {
+      document.querySelector('.hios-active').className = 'hios-inactive';
+      var containerName = skin + 'Container';
+      selectors[containerName].className = 'hios-active';
+    },
+    /**
+     * updateTrackList
+     * renders initial track list when APP.reset() and otherwise moves hios-playing class
+     * re-renders currently playing info with populateCurrentTrack private method
+     * @param {Object} options hash
+     */
+    updateTrackList: function(options) {
+      var list = selectors.trackList
+      console.log(list)
+      console.log(selectors)
+      var nodeIndex = options.track.id - 1
+      var previousNode = document.querySelector('.hios-playing')
+      if (previousNode) previousNode.className = 'hios-track';
+
+      if (options.track && options.music){
+        list.innerHTML = '';
+        options.music.forEach(function (track){
+          var node =  document.createElement('div');
+          node.className = 'hios-track';
+          node.setAttribute('data-state', 'next');
+          node.innerHTML = trackHTML;
+          node.querySelector('.hios-song-title').innerHTML = track.title;
+          node.querySelector('.hios-song-artist').innerHTML = track.artist;
+          node.querySelector('.hios-thumbnail').setAttribute('src', track.image);
+          list.appendChild(node);
+        })
+      };
+
+      list.childNodes[nodeIndex].className = 'hios-track hios-playing'
+      populateCurrentTrack(options.track)
+    },
+  };
+};
