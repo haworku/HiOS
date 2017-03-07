8291e381d8043f156125c417f29028dad5111e78
haworku <haworku@gmail.com>
2017-03-07 12:30:31 -0600

    cleanup

--- js/redux-app.js
+++ js/redux-app.js
@@ -1,4 +1,6 @@
 'use strict';
+ // window.hios = Object.create(null);
+ 
 let APP = {};
 
 APP.launch = function (music) { // this method is called from musicupload.js
@@ -10,7 +12,7 @@ APP.launch = function (music) { // this method is called from musicupload.js
   APP.actions = hiosActions(APP.store);
   APP.audio = hiosAudio();
   APP.view = hiosView(APP.actions, APP.audio);
-  APP.hiosBind = bindActionCreators(APP.actions, APP.store.dispatch)
+  APP.hiosBind = bindActionCreators(APP.actions, APP.store.dispatch);
   APP.view.buildHTML(); 
   observeStore(APP.store, APP.view.updateView);
   APP.store.dispatch({type: 'LOAD_PLAYER', uploadedMusic: music});   
