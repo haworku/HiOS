e55ea5c29ecdae573759ac1bd6145dae82a0469f
haworku <haworku@gmail.com>
2017-03-07 14:39:24 -0600

    clear out redux-from-scratch stuff -adding redux to app

--- js/store.js
+++ js/store.js
@@ -1,24 +1,3 @@
-const createStore = (combinedReducer) => {
-  console.log('creating store')
-  let state;
-  let listeners = [];
-
-  const getState = () => state; 
-
-  const dispatch = (action)  => {
-    console.log('listeners', listeners)
-    state = combinedReducer(state, action);
-    listeners.forEach( listener => listener() ); 
-  }
-
-  const subscribe = (listener) => {
-    listeners.push(listener);
-    return () => {
-      listeners = listeners.filter( l => l !== listener ) // to remove listener subscribe again
-    }
-  }
-
-  dispatch({}) // initial state rendered when store created
-
-  return { getState, dispatch, subscribe};
-};
+import { createStore } from 'redux'
+import hiosApp from './reducers'
+let store = createStore(hiosApp)
